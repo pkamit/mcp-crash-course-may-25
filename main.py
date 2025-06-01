8e6de06a01d5a7c53e2ed5e9409070132a7f70a7
pkamit <amit.pandey812@gmail.com>
2025-06-01 16:33:38 +0530

    Enhance main.py with Gradio integration and update server implementations
    - Refactored main.py to implement a Gradio chat interface for user interaction.
    - Integrated MultiServerMCPClient to handle requests for both math and weather servers.
    - Updated math_server.py and weather_server.py to ensure compatibility with the new client structure.
    - Added Gradio and Black as dependencies in pyproject.toml, and updated uv.lock accordingly.

--- main.py
+++ main.py
@@ -1,31 +1,51 @@
 import asyncio
 import os
 
+import gradio as gr
 from dotenv import load_dotenv
+from langchain_core.messages import AIMessage, HumanMessage , SystemMessage
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import create_react_agent
 from mcp import ClientSession, StdioServerParameters
-
 from mcp.client.stdio import stdio_client
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
-
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
-llm = ChatOpenAI()
-
 
 studio_server_parameters = StdioServerParameters(
     command="python",
     args=["/home/amit/Documents/gitprojects/mcp-crash-course/servers/math_server.py"],
 )
 
-stdio_client = stdio_client(studio_server_parameters)
 
-async def main():
-    print("Hello from mcp-crash-course!")
+async def agent_response(message, history):
+    client = MultiServerMCPClient({
+        "math": {
+            "command": "python",
+            "args": ["/home/amit/Documents/gitprojects/mcp-crash-course/servers/math_server.py"],
+            "transport": "stdio",
+        },
+        "weather": {
+            "url": "http://localhost:8000/mcp",
+            "transport": "streamable_http",
+        }
+    })
+    llm = ChatOpenAI(model="gpt-4-turbo", temperature=0.0, api_key=OPENAI_API_KEY)
+    tools = await client.get_tools()
+    agent = create_react_agent(llm, tools)
+    # Add prompt as SystemMessage if needed
+    messages = [SystemMessage(content="You are a helpful assistant."), HumanMessage(content=message)]
+    agent_response = await agent.ainvoke({"messages": messages})
+    return agent_response["messages"][-1].content
 
 
-if __name__ == "__main__":
-    asyncio.run(main()) 
+
+def gradio_chat(message, history):
+    return asyncio.run(agent_response(message, history))
+
+
+ui = gr.ChatInterface(gradio_chat)
+ui.launch()
