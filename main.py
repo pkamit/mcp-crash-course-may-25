3326adb3d2371a30206aabbc2c984a1b567e8f7e
pkamit <amit.pandey812@gmail.com>
2025-06-01 12:37:58 +0530

    Add OpenAI integration and update dependencies
    - Enhanced main.py to include OpenAI API integration and set up a standard input/output client for the math server.
    - Updated pyproject.toml to include isort as a dependency.
    - Modified uv.lock to reflect the addition of isort version 6.0.1.
    - Added a blank line in weather_server.py for consistency.

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 import asyncio
+import os
+
+from dotenv import load_dotenv
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+
+from mcp.client.stdio import stdio_client
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+llm = ChatOpenAI()
+
+
+studio_server_parameters = StdioServerParameters(
+    command="python",
+    args=["/home/amit/Documents/gitprojects/mcp-crash-course/servers/math_server.py"],
+)
+
+stdio_client = stdio_client(studio_server_parameters)
 
 async def main():
     print("Hello from mcp-crash-course!")
